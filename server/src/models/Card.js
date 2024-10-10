10e748f1778de734fd5d58cbfd095a77751d28ca
apatni123 <aishah.patni@barclays.com>
2024-10-10 15:07:42 +0100

    updated

---
+++ server/src/models/Card.js
@@ -0,0 +1,37 @@
+const { db, DataTypes, Model } = require("../db/config"); 
+
+// Define the Deck model
+class Card extends Model {}
+
+// Initialize the User model
+Card.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true, // Set primary key 
+        autoIncrement: true // Automatically increment the id
+    },
+    name: {
+        type: DataTypes.STRING,
+        
+    },
+    mojo: {
+        type: DataTypes.INTEGER,
+        
+    },
+    stamina: {
+        type: DataTypes.INTEGER,
+        
+    },
+    imgURL: {
+        type: DataTypes.STRING,
+        
+    },
+}, {
+    sequelize: db,
+    modelName: "card"
+});
+
+
+
+// Export the User model
+module.exports = Card;
---
+++ server/src/models/Deck.js
@@ -0,0 +1,29 @@
+const { db, DataTypes, Model } = require("../db/config"); 
+
+// Define the Deck model
+class Deck extends Model {}
+
+// Initialize the User model
+Deck.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true, // Set primary key 
+        autoIncrement: true // Automatically increment the id
+    },
+    name: {
+        type: DataTypes.STRING,
+        
+    },
+    xp: {
+        type: DataTypes.INTEGER,
+        
+    },
+}, {
+    sequelize: db,
+    modelName: "deck"
+});
+
+
+
+// Export the User model
+module.exports = Deck;
