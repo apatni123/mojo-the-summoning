26070b2d9b7c16c2eb5406664321f5b5d093fb6e
apatni123 <aishah.patni@barclays.com>
2024-10-10 14:40:41 +0100

    updated

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,25 @@
-// create your User model here
+const { db, DataTypes, Model } = require("../db/config"); 
+
+// Define the User model
+class User extends Model {}
+
+// Initialize the User model
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true, // Set primary key 
+        autoIncrement: true // Automatically increment the id
+    },
+    username: {
+        type: DataTypes.STRING,
+        
+    },
+}, {
+    sequelize: db,
+    modelName: "user"
+});
+
+
+
+// Export the User model
+module.exports = User;
